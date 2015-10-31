9f2ac3943d1318633a186a63a3f3fd9b19dce024
Duyet Nguyen <duyetlnguyen@gmail.com>
2015-10-31 02:43:35 -0700

    Add code to construct dependency graph

--- concurrent_commands.c
+++ concurrent_commands.c
@@ -11,14 +11,15 @@ struct dependency
   struct command *cmd;
   char **reads;
   char **writes;
-  int *waiters;
-  int *blockers;
+  int **waiters;
+  int **blockers;
 };
+
+typedef struct dependency *dependency_t;
 
 #define TRUE 1
 #define FALSE 0
-
-typedef struct dependency *dependency_t;
+#define malloc_pointer(type, count) ((type *) malloc ((count) * sizeof (type)))
 
 char **
 merge (char **first, char **second)
@@ -38,7 +39,7 @@ merge (char **first, char **second)
 
   if (count > 0)
     {
-      tmp = (char **) malloc ((count + 1) * sizeof (char *));
+      tmp = malloc_pointer (char *, count + 1);
       tmp[count] = NULL;
       while (first && *first)
         *tmp++ = *first++;
@@ -90,7 +91,7 @@ get_reads (command_t cmd)
           if (cmd->input)
             {
               first = result;
-              second = (char **) malloc (2 * sizeof (char *));
+              second = malloc_pointer (char *, 2);
               second[1] = NULL;
               second[0] = cmd->input;
               result = merge (first, second);
@@ -144,7 +145,7 @@ get_writes (command_t cmd)
           if (cmd->output)
             {
               first = result;
-              second = (char **) malloc (2 * sizeof (char *));
+              second = malloc_pointer (char *, 2);
               second[1] = NULL;
               second[0] = cmd->output;
               result = merge (first, second);
@@ -163,12 +164,85 @@ get_writes (command_t cmd)
   return result;
 }
 
+int
+intersect (char **first, char **second)
+{
+  char **it1;
+  char **it2;
+
+  it1 = first;
+  while (it1 && *it1)
+    {
+      it2 = second;
+      while (it2 && *it2)
+        {
+          if (strcmp (*it1, *it2) == 0)
+            return TRUE;
+          it2++;
+        }
+      it1++;
+    }
+
+  return FALSE;
+}
+
+int **
+create_int_pointers_array (int count, int fill)
+{
+  int **arr = malloc_pointer (int *, count + 1);
+  arr[count] = NULL;
+  while (*arr)
+    {
+      *arr = malloc_pointer (int, 1);
+      **arr = fill;
+      arr++;
+    }
+  return arr - count;
+}
+
+void
+destroy_int_pointers_array (int **arr)
+{
+  int **it = arr;
+  while (*it)
+    {
+      free (*it);
+      it++;
+    }
+  free (arr);
+}
+
+dependency_t
+create_dependency (command_t cmd, int index, int count)
+{
+  dependency_t dep = malloc_pointer (struct dependency, 1);
+  dep->index = index;
+  dep->cmd = cmd;
+  dep->reads = get_reads (cmd);
+  dep->writes = get_writes (cmd);
+  dep->waiters = create_int_pointers_array (count, FALSE);
+  dep->blockers = create_int_pointers_array (count, FALSE);
+  return dep;
+}
+
+void
+destroy_dependency (dependency_t dep)
+{
+  free (dep->reads);
+  free (dep->writes);
+  destroy_int_pointers_array (dep->waiters);
+  destroy_int_pointers_array (dep->blockers);
+  free (dep);
+}
+
 dependency_t *
 create_dependencies (command_stream_t strm)
 {
   int count;
   int index;
-  dependency_t *dep;
+  dependency_t *deps;
+  dependency_t *it1;
+  dependency_t *it2;
   command_t cmd;
 
   count = 0;
@@ -179,39 +253,48 @@ create_dependencies (command_stream_t strm)
     }
   reset_command_stream (strm);
 
-  dep = (dependency_t *) malloc ((count + 1) * sizeof (dependency_t));
-  dep[count] = NULL;
+  deps = malloc_pointer (dependency_t, count + 1);
+  deps[count] = NULL;
 
   index = 0;
   while ((cmd = current_command (strm)))
     {
-      dependency_t cdep = (dependency_t) malloc (sizeof (struct dependency));
-      cdep->index = index;
-      cdep->cmd = cmd;
-      cdep->reads = get_reads (cmd);
-      cdep->writes = get_writes (cmd);
-
-      dep[index] = cdep;
-
+      deps[index] = create_dependency (cmd, index, count);
       index++;
       next_command (strm);
     }
 
-  return dep;
+  it1 = deps;
+  while (it1 && *it1)
+    {
+      it2 = deps;
+      while (it2 && it2 && *it2 != *it1)
+        {
+          dependency_t dep1 = *it1;
+          dependency_t dep2 = *it2;
+          if (intersect (dep2->writes, dep1->reads) || // Read-After-Write
+            intersect (dep2->reads, dep1->writes) || // Write-After-Read
+            intersect (dep2->writes, dep1->writes)) // Write-After-Write
+            {
+              *(dep1->waiters[dep2->index]) = TRUE;
+              *(dep2->blockers[dep1->index]) = TRUE;
+            }
+          it2++;
+        }
+      it1++;
+    }
+
+  return deps;
 }
 
 void
-destroy_dependencies (dependency_t *dep)
+destroy_dependencies (dependency_t *deps)
 {
-  dependency_t *tmp = dep;
+  dependency_t *tmp = deps;
   while (tmp)
   {
-    free ((*tmp)->reads);
-    free ((*tmp)->writes);
-    free ((*tmp)->waiters);
-    free ((*tmp)->blockers);
-    free ((*tmp));
+    destroy_dependency (*tmp);
     tmp++;
   }
-  free (dep);
+  free (deps);
 }
