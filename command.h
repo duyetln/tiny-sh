eafd693c5ea76547173043e07a494aba63ed4ed6
Duyet Nguyen <duyetlnguyen@gmail.com>
2015-11-03 14:01:32 -0800

    Update command stream parser

--- command.h
+++ command.h
@@ -1,17 +1,35 @@
 #ifndef COMMAND_H
 #define COMMAND_H
 
+enum io_op
+{
+  IO_INPUT,      // "<"
+  IO_DUPIN,      // "<&"
+  IO_IODUAL,     // "<>"
+  IO_OUTPUT,     // ">"
+  IO_DUPOUT,     // ">&"
+  IO_APPEND,     // ">>"
+  IO_CLOBBER,    // ">|"
+};
+
+struct io_node
+{
+  enum io_op op;
+  char *io_num;
+  char *word;
+  struct io_node *next;
+};
+
 enum command_type
 {
   CMD_AND,        // A && B
   CMD_SEQUENCE,   // A ; B
   CMD_OR,         // A || B
-  CMD_PIPE,        // A | B
+  CMD_PIPE,       // A | B
   CMD_SIMPLE,     // a simple command
   CMD_SUBSHELL,   // ( A )
 };
 
-// Data associated with a command.
 struct command
 {
   enum command_type type;
@@ -19,23 +37,17 @@ struct command
   // Exit status, or -1 if not known (e.g., because it has not exited yet).
   int status;
 
-  // I/O redirections, or 0 if none.
-  char *input;
-  char *output;
+  struct io_node *io_head;
+  struct io_node *io_tail;
 
   union
   {
-    // for AND_COMMAND, SEQUENCE_COMMAND, OR_COMMAND, PIPE_COMMAND:
     struct command *command[2];
-
-    // for SIMPLE_COMMAND:
-    char **word;
-
-    // for SUBSHELL_COMMAND:
     struct command *subshell_command;
+    char **word;
   } u;
 };
 
 typedef struct command *command_t;
-
+typedef struct io_node *io_node_t;
 #endif
--- command_stream.c
+++ command_stream.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <unistd.h>
 
 #include <error.h>
 
@@ -11,6 +12,8 @@
 #define malloc_command ((command_t) malloc (sizeof (struct command)))
 #define malloc_command_node ((command_node_t) malloc (sizeof (struct command_node)))
 #define malloc_command_stream ((command_stream_t) malloc (sizeof (struct command_stream)))
+#define istkn(tkn, tp) ((tkn) != NULL && (tkn)->type == tp)
+#define isiotkn(t) (istkn (t, TKN_INPUT) || istkn (t, TKN_DUPIN) || istkn (t, TKN_IODUAL) || istkn (t, TKN_OUTPUT) || istkn (t, TKN_DUPOUT) || istkn (t, TKN_APPEND) || istkn (t, TKN_CLOBBER))
 
 command_t
 parse_command_sequence (token_stream_t strm);
@@ -21,8 +24,8 @@ create_command (enum command_type t)
   command_t cmd = malloc_command;
   cmd->type = t;
   cmd->status = -1;
-  cmd->input = NULL;
-  cmd->output = NULL;
+  cmd->io_head = NULL;
+  cmd->io_tail = NULL;
 
   if (t == CMD_SIMPLE)
     cmd->u.word = NULL;
@@ -40,7 +43,7 @@ create_command (enum command_type t)
 void
 assert_token (token_stream_t strm, enum token_type t)
 {
-  if (current_token (strm)->type != t)
+  if (!istkn (current_token (strm), t))
     {
       char *tw;
       switch (t)
@@ -74,7 +77,7 @@ parse_simple_command (token_stream_t strm)
   // printf("parse simple command: %s\n", current_token (strm)->value);
   int c = 0;
   int i = 0;
-  while (current_token (strm)->type == TKN_WORD)
+  while (istkn (current_token (strm), TKN_WORD))
     {
       c++;
       next_token (strm);
@@ -84,7 +87,7 @@ parse_simple_command (token_stream_t strm)
   cmd->u.word = (char**) malloc ((c + 1) * sizeof (char *));
 
   backward_token_stream (strm, c);
-  while (current_token (strm)->type == TKN_WORD)
+  while (istkn (current_token (strm), TKN_WORD))
     {
       cmd->u.word[i] = strdup (current_token (strm)->value);
       i++;
@@ -99,21 +102,72 @@ command_t
 parse_io_redirection (token_stream_t strm, command_t cmd)
 {
   // printf("parse io redirection: %s\n", current_token (strm)->value);
+  io_node_t io;
+  io = (io_node_t) malloc (sizeof (struct io_node));
+  io->io_num = NULL;
+  io->word = NULL;
+  io->next = NULL;
+
   token_t c = current_token (strm);
   token_t n = next_token (strm);
-  char **w = NULL;
 
-  assert_token (strm, TKN_WORD);
+  if (istkn (c, TKN_IONUMBER))
+    {
+      io->io_num = strdup (c->value);
+      c = current_token (strm);
+      n = next_token (strm);
+    }
 
-  if (c->type == TKN_INPUT)
-    w = &(cmd->input);
-  else if (c->type == TKN_OUTPUT)
-    w = &(cmd->output);
+  if (istkn (c, TKN_INPUT))
+    {
+      assert_token (strm, TKN_WORD);
+      io->op = IO_INPUT;
+    }
+  else if (istkn (c, TKN_DUPIN))
+    {
+      assert_token (strm, TKN_IONUMBER);
+      io->op = IO_DUPIN;
+    }
+  else if (istkn (c, TKN_IODUAL))
+    {
+      assert_token (strm, TKN_WORD);
+      io->op = IO_IODUAL;
+    }
+  else if (istkn (c, TKN_OUTPUT))
+    {
+      assert_token (strm, TKN_WORD);
+      io->op = IO_OUTPUT;
+    }
+  else if (istkn (c, TKN_DUPOUT))
+    {
+      assert_token (strm, TKN_IONUMBER);
+      io->op = IO_DUPOUT;
+    }
+  else if (istkn (c, TKN_APPEND))
+    {
+      assert_token (strm, TKN_WORD);
+      io->op = IO_APPEND;
+    }
+  else if (istkn (c, TKN_CLOBBER))
+    {
+      assert_token (strm, TKN_WORD);
+      io->op = IO_CLOBBER;
+    }
+  else if (io->io_num != NULL)
+    error (1, 0, "%d: expecting io token\n", c->line);
 
-  // handle the case where there are more than <'s or >'s
-  if (*w != NULL)
-    free (*w);
-  *w = strdup (n->value);
+  io->word = strdup (n->value);
+
+  if (cmd->io_head == NULL || cmd->io_tail == NULL)
+    {
+      cmd->io_head = io;
+      cmd->io_tail = io;
+    }
+  else
+    {
+      cmd->io_tail->next = io;
+      cmd->io_tail = io;
+    }
 
   next_token (strm);
 
@@ -143,15 +197,15 @@ parse_command (token_stream_t strm)
   command_t cmd;
   token_t t = current_token (strm);
 
-  if (t->type == TKN_WORD)
+  if (istkn (t, TKN_WORD))
     cmd = parse_simple_command (strm);
-  else if (t->type == TKN_OPENPAREN)
+  else if (istkn (t, TKN_OPENPAREN))
     cmd = parse_subshell_command (strm);
   else
     error (1, 0, "%d: expecting word or '(' token\n", t->line);
 
   t = current_token (strm);
-  while (t->type == TKN_INPUT || t->type == TKN_OUTPUT)
+  while (istkn (t, TKN_IONUMBER) || isiotkn (t))
     {
       cmd = parse_io_redirection (strm, cmd);
       t = current_token (strm);
@@ -172,7 +226,7 @@ parse_pipelines (token_stream_t strm)
   lft = parse_command (strm);
   t = current_token (strm);
 
-  while (t->type == TKN_PIPE)
+  while (istkn (t, TKN_PIPE))
     {
       next_token (strm);
 
@@ -200,13 +254,13 @@ parse_logicals (token_stream_t strm)
   lft = parse_pipelines (strm);
   t = current_token (strm);
 
-  while (t->type == TKN_AND || t->type == TKN_OR)
+  while (istkn (t, TKN_AND) || istkn (t, TKN_OR))
     {
       next_token (strm);
 
-      if (t->type == TKN_AND)
+      if (istkn (t, TKN_AND))
         lgcl = create_command (CMD_AND);
-      else if (t->type == TKN_OR)
+      else if (istkn (t, TKN_OR))
         lgcl = create_command (CMD_OR);
 
       rgt = parse_pipelines (strm);
@@ -230,7 +284,7 @@ parse_command_sequence (token_stream_t strm)
 
   lft = parse_logicals (strm);
 
-  while (current_token (strm)->type == TKN_SEMICOLON)
+  while (istkn (current_token (strm), TKN_SEMICOLON))
     {
       next_token (strm);
       rgt = parse_logicals (strm);
@@ -276,11 +330,11 @@ parse (token_stream_t strm)
   cmd_strm->tail = NULL;
   cmd_strm->curr = NULL;
 
-  while (current_token (strm)->type != TKN_EOF)
+  while (!istkn (current_token (strm), TKN_EOF))
     {
       add_command (cmd_strm, parse_command_sequence (strm));
 
-      while (current_token (strm)->type == TKN_DBLSEMICLN)
+      while (istkn (current_token (strm), TKN_DBLSEMICLN))
         {
           next_token (strm);
           add_command (cmd_strm, parse_command_sequence (strm));
@@ -333,10 +387,20 @@ destroy_command (command_t cmd)
         }
     }
 
-  if (cmd->input != NULL)
-    free (cmd->input);
-  if (cmd->output != NULL)
-    free (cmd->output);
+  io_node_t c;
+  io_node_t n;
+
+  c = cmd->io_head;
+  while (c != NULL)
+    {
+      if (c->io_num)
+        free (c->io_num);
+      free (c->word);
+
+      n = c->next;
+      free (c);
+      c = n;
+    }
 
   free (cmd);
 }
